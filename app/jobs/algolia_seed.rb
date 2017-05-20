dbc3712563a39df7dd46c1d8cd1aa4c4ca9872ca
mathcoch <mathieu@cochet.me>
2017-05-20 14:43:06 +0200

    Algolia included

--- app/jobs/algolia_seed.rb
+++ app/jobs/algolia_seed.rb
@@ -1,8 +1,17 @@
+# Algolia.init :application_id => "LZEMPFONKV", :api_key => "d273e7cc48a855feae0750a9fac8db9a"
+# def load_data_from_database
+#   records = Profile.all.map { |pr| {username: pr.username, bio: pr.bio, img_url: pr.img_url, phone: pr.phone, email: pr.email, linkedin: pr.linkedin, score: pr.score} }
+# end
+# index = Algolia::Index.new("Profile")
+# load_data_from_database.each_slice(1000) do |batch|
+#   index.add_objects(batch)
+# end
+
 Algolia.init :application_id => "LZEMPFONKV", :api_key => "d273e7cc48a855feae0750a9fac8db9a"
 def load_data_from_database
-  records = Profile.all.map { |pr| {username: pr.username, bio: pr.bio, img_url: pr.img_url, phone: pr.phone, email: pr.email, linkedin: pr.linkedin, score: pr.score} }
+  records = Post.all.map { |pr| {body: pr.body, score: pr.score, username: pr.user.username} }
 end
-index = Algolia::Index.new("Profile")
+index = Algolia::Index.new("Post")
 load_data_from_database.each_slice(1000) do |batch|
   index.add_objects(batch)
 end
