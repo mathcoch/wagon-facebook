736bc2866f7ab08e89adb08002f48ddd9f322ccc
mathcoch <mathieu@cochet.me>
2017-05-21 19:30:50 +0200

    Add JS on button

--- app/assets/javascripts/load_on_top.js
+++ app/assets/javascripts/load_on_top.js
@@ -1,3 +1,10 @@
 // $(document).ready(function(){
 //     $(this).scrollTop(0);
 // });
+
+$(document).ready(function(){
+    $('#start_search').on('click', function(event){
+      $('main').addClass('hidden');
+    });
+});
+
