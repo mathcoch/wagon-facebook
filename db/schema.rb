dbf49f0563efef2e04bbbd307f11ca842364f15b
mathcoch <mathieu@cochet.me>
2017-05-21 15:29:42 +0200

    Former password columns deleted

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170520184911) do
+ActiveRecord::Schema.define(version: 20170521132845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,8 +39,6 @@ ActiveRecord::Schema.define(version: 20170520184911) do
   create_table "users", force: :cascade do |t|
     t.string   "email"
     t.string   "username"
-    t.string   "password_hash"
-    t.string   "password_salt"
     t.integer  "profile_id"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
